9da872366748d2ebe14116bea356d71ba2422536
dmaslov <masdmtr@gmail.com>
2017-12-08 00:19:25 +0300

    config server implemented

--- src/main/java/io/dmitr/skillsmonster/EurekaRegistartionCoreApplication.java
+++ src/main/java/io/dmitr/skillsmonster/EurekaRegistartionCoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+
 public class EurekaRegistartionCoreApplication {
 
     public static void main(String[] args) {
